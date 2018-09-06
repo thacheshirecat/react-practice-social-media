4b28b091f205f135223ccddc066484c399d0c681
Guest User <Guest@epicodus-55.local>
2018-09-06 10:16:40 -0700

    Fix the title.

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ module.exports = {
     new HtmlWebpackPlugin({
      template:'template.ejs',
      appMountId: 'react-app-root',
-     title: 'React Help Queue',
+     title: 'React Social Media',
      filename: resolve(__dirname, "build", "index.html"),
    })
   ]
